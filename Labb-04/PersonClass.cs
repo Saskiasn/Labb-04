c5c12b4b781ce7987476bb8fa9888fdd93463921
Saskiasn <h24sasni@du.se>
2025-01-07 12:42:04 +0100

    Sprint 1 done

---
+++ Labb-04/PersonClass.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using static Labb_04.Program;
+
+namespace Labb_04
+{
+    public enum Gender
+    {
+        Kvinna,
+        Man,
+        IckeBinär,
+        Annan
+    }
+
+    public struct Hair
+    {
+        public string HairLenght { get; set; }
+        public string HairColor { get; set; }
+
+        public Hair(string hairlenght, string haircolor)
+        {
+            HairLenght = hairlenght;
+            HairColor = haircolor;
+        }
+
+        public override string ToString()
+        {
+            return $"Hårlängd: {HairLenght}, Haircolor: {HairColor}";
+        }
+    }
+
+    public class Person
+    {
+        // Egenskaper
+        public string Name { get; private set; }
+        public Gender Gender { get; private set; }
+        public Hair Hair { get; private set; }
+        public DateTime Birthday { get; private set; }
+        public string Eyecolor { get; private set; }
+
+        // Konstruktor
+        public Person(string name, Gender gender, Hair hair, DateTime birthday, string eyecolor)
+        {
+            Name = name;
+            Gender = gender;
+            Hair = hair;
+            Birthday = birthday;
+            Eyecolor = eyecolor;
+        }
+
+        // ToString-metod
+        public override string ToString()
+        {
+            return $"Namn: {Name}\n" +
+                   $"Kön: {Gender}\n" +
+                   $"Hår: {Hair}\n" +
+                   $"Födelsedag: {Birthday:yyyy-MM-dd}\n" +
+                   $"Ögonfärg: {Eyecolor}";
+        }
+    }
+}
+
--- Labb-04/Program.cs
+++ Labb-04/Program.cs
@@ -4,7 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("test test saskia!");
+        // Skapa en instans av Hair
+        Hair personHair = new Hair("Långt", "Blond");
+
+        // Skapa en instans av Person
+        Person person = new Person(
+            "Karl Johansson",     // Namn
+            Gender.Man,           // Kön
+            personHair,           // Hår
+            new DateTime(1985, 3, 25), // Födelsedag
+            "Grön"                // Ögonfärg
+        );
+        Console.WriteLine(person.name);
+
+        //Person person = new Person();
+
+        // Skriv ut information om personen
+        Console.WriteLine(person.ToString());
+
     }
+
+
+
+
+
 }
 
