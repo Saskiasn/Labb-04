d1a5bbce1cbd5968a09fd5171678bfed9af74d6a
Saskiasn <h24sasni@du.se>
2025-01-09 15:45:35 +0100

    Tog bort personen från sprint 1

--- Labb-04/Program.cs
+++ Labb-04/Program.cs
@@ -7,20 +7,6 @@ class Program
 
     static void Main(string[] args)
     {
-        // Skapa en instans av Hair
-        Hair personHair = new Hair("Långt", "Blond");
-
-        // Skapa en instans av Person
-        Person person = new Person(
-            "Karl Johansson",     // Namn
-            Gender.Man,           // Kön
-            personHair,           // Hår
-            new DateTime(1985, 3, 25), // Födelsedag
-            "Grön"                // Ögonfärg
-        );
-
-        // Skriv ut information om personen
-        Console.WriteLine(person.ToString());
 
         Console.WriteLine("Listan för personer är nu skapad. Vi kan fortsätta med nästa steg!");
 
